6e53dacf4e4e680d3d303fcf466f5a104d5c6afa
shahidulgod <shahidulgod@gmail.com>
2023-08-20 22:06:53 +0600

    addind liitem

--- calculation.js
+++ calculation.js
@@ -1,4 +1,12 @@
 
+function itemNameShow(target){
+  const selectedItemContainer= document.getElementById("selectItem");
+  const  itemName = target.parentNode.childNodes[1].innerText;
+  const li = document.createElement("li");
+  li.innerText = itemName;
+  selectedItemContainer.appendChild(li);
+}
+
 
 
 
